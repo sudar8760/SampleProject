4f46b454374be7a4cc233af698a86d41494eb8e2
sudar8760 <sudar.rjsr@gmail.com>
2021-08-10 19:46:20 +0530

    first push

---
+++ src/main/java/com/sample/Match.java
@@ -0,0 +1,52 @@
+package com.sample;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.SystemOutLogger;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Match {
+	public static void main(String[] args) throws IOException  {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Register.html");
+		 List<WebElement> findElements = driver.findElements(By.id("countries"));
+		 for (int i = 0; i < findElements.size(); i++) {
+			
+		
+		WebElement element = findElements.get(i);
+		String text = element.getText();
+		System.out.println(text);
+		
+	
+		
+		
+		
+		 }
+		
+		
+	}
+	
+	}
+		
+		
+		
+			
